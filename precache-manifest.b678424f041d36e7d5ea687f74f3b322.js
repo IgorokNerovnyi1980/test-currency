418b7d7b1e98ef7d6d0eabb8cc3fe228e66fed58
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2019-10-10 12:49:14 +0300

    Updates

---
+++ precache-manifest.b678424f041d36e7d5ea687f74f3b322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60223f067a78d7d0e341e17637a2ef7f",
+    "url": "/test-currency/index.html"
+  },
+  {
+    "revision": "db6a6e2b9172874744f1",
+    "url": "/test-currency/static/css/main.5624aa5a.chunk.css"
+  },
+  {
+    "revision": "ebf0ec8e918433a8ff5e",
+    "url": "/test-currency/static/js/2.d3e200b3.chunk.js"
+  },
+  {
+    "revision": "db6a6e2b9172874744f1",
+    "url": "/test-currency/static/js/main.c0c487e0.chunk.js"
+  },
+  {
+    "revision": "6344063f4c92e5ce5e9c",
+    "url": "/test-currency/static/js/runtime-main.8463ff11.js"
+  }
+]);
